14f65e7d906e9595cf93657bd6a58fa0401dc1f4
Yury Bakhmutski <YURY_BAKHMUTSKI@epam.com>
2017-06-27 10:48:17 +0300

    [PPP-3735] - Use of vulnerable component httpclient-3.0.1.jar, commons-httpclient-3.1 - httpclient-4.0.1.jar, SONATYPE-2007-0004, CVE-2012-6153, CVE-2011-1498, CVE-2014-3577

--- kettle-sdk-embedding-samples/src/main/java/org/pentaho/di/sdk/samples/carte/GetTransStatusSample.java
+++ kettle-sdk-embedding-samples/src/main/java/org/pentaho/di/sdk/samples/carte/GetTransStatusSample.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2016 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2017 by Pentaho : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -23,50 +23,57 @@
 package org.pentaho.di.sdk.samples.carte;
 
 import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.message.BasicHeader;
 import org.pentaho.di.core.Const;
+import org.pentaho.di.core.util.HttpClientManager;
+import org.pentaho.di.core.util.HttpClientUtil;
 import org.pentaho.di.www.GetTransStatusServlet;
 
-public class GetTransStatusSample {
+public class GetTransStatusSample extends AbstractSample {
 
   public static void main( String[] args ) throws Exception {
     if ( args.length < 5 ) {
       System.out.println( " You must specify the following parameters Carte_host Carte_port "
-          + "Carte_login Carte_password job_name" );
+        + "Carte_login Carte_password job_name" );
       System.out.println( " For example 127.0.0.1 8088 cluster cluster my_job" );
       System.exit( 1 );
     }
+    init( args[ 0 ], Integer.parseInt( args[ 1 ] ), args[ 2 ], args[ 3 ] );
     // building target url
-    String realHostname = args[0];
-    String port = args[1];
+    String realHostname = args[ 0 ];
+    String port = args[ 1 ];
 
     String urlString = "http://" + realHostname + ":" + port + GetTransStatusServlet.CONTEXT_PATH
-        + "?xml=Y&name=" + args[4];
+      + "?xml=Y&name=" + args[ 4 ];
     urlString = Const.replace( urlString, " ", "%20" );
 
     //building auth token
-    String plainAuth = args[2] + ":" + args[3];
+    String plainAuth = args[ 2 ] + ":" + args[ 3 ];
     String auth = "Basic " + Base64.encodeBase64String( plainAuth.getBytes() );
 
     sendGetTransStatusRequest( urlString, auth );
   }
 
   public static void sendGetTransStatusRequest( String urlString, String authentication ) throws Exception {
-    GetMethod method = new GetMethod( urlString );
-    method.setDoAuthentication( true );
+    HttpGet method = new HttpGet( urlString );
+    HttpClientContext context = HttpClientUtil.createPreemptiveBasicAuthentication( host, port, user, password );
     //adding authorization token
     if ( authentication != null ) {
-      method.addRequestHeader( new Header( "Authorization", authentication ) );
+      method.addHeader( new BasicHeader( "Authorization", authentication ) );
     }
 
     //executing method
-    HttpClient client = new HttpClient(  );
-    int code = client.executeMethod( method );
-    String response = method.getResponseBodyAsString();
+    HttpClient client = HttpClientManager.getInstance().createDefaultClient();
+    HttpResponse httpResponse = context != null ? client.execute( method, context ) : client.execute( method );
+    int code = httpResponse.getStatusLine().getStatusCode();
+    String response = HttpClientUtil.responseToString( httpResponse );
     method.releaseConnection();
-    if ( code >= 400 ) {
+    if ( code >= HttpStatus.SC_BAD_REQUEST ) {
       System.out.println( "Error occurred during getting job status." );
     }
     System.out.println( "Server response:" );
